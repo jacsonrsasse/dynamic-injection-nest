fba759a9ddde3e93dba4448dc5a5748ea3fd52e8
Jacson <jacson.sasse@hotmail.com>
2023-07-26 02:26:07 -0300

    refactor: removed unused files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ParteQueEnviaModule } from './modules/parte-que-envia/parte-que-envia.module';
 
 @Module({
   imports: [ParteQueEnviaModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
